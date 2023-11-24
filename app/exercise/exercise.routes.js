394d37e7eaf72f506f0c4b0e5ff7299cba2a6ee8
I.Safonov <safonovil17@yandex.ru>
2023-11-24 17:33:28 +0300

    Added logging of workouts and exercises

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -8,13 +8,17 @@ import {
 	getExercises,
 	updateExercise
 } from './exercise.controller.js'
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
 
 const router = express.Router()
 
 router.route('/').post(protect, createNewExercise).get(protect, getExercises)
+
 router
 	.route('/:id')
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
+
+router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
 
 export default router
