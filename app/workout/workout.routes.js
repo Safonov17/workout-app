c72d3db0ea4ec852d4686eab6f7d3dadfd212359
I.Safonov <safonovil17@yandex.ru>
2023-11-25 15:38:28 +0300

    Implemented creation of workout log

--- app/workout/workout.routes.js
+++ app/workout/workout.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
+import { createNewWorkoutLog } from './log/workout-log.controller.js'
 import {
 	createNewWorkout,
 	deleteWorkout,
@@ -17,5 +18,7 @@ router
 	.get(protect, getWorkouts)
 	.put(protect, updateWorkout)
 	.delete(protect, deleteWorkout)
+
+router.route('/log/:id').post(protect, createNewWorkoutLog)
 
 export default router
